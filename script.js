037ff1f5f560c6da5dafb08df0ad05d282840582
rwba7567 <rwba7567@gmail.com>
2022-02-25 20:02:11 +0800

    ALPHA Build

--- script.js
+++ script.js
@@ -30,13 +30,43 @@ function operate(string){
 
     //search for addition
     console.log("debug:"+ string)
-    while (String(string).search(/[+x÷-]+/g) != -1)
+    console.log("checksum: "+String(string).search(/[+x÷-]+/g))
+    while (String(string).search(/[+x÷-]+/g) > -1)
     {   
-        pos = string.search(/[+x÷-]+/g);
-        if (pos != -1)
+        console.log("hi")
+
+        string = String(string);
+
+        if (string.search(/[+x÷-]+/g) == 0)
         {
-            num1 = string.slice(0,pos);
-            string = string.slice(pos);
+            pos = string.slice(1).search(/[+x÷-]+/g) + 1;
+
+            if (pos==0)
+            {
+                break;
+            }
+            console.log("New string: "+ string.slice(1))
+            console.log("New pos: "+ pos)
+        }
+        else{
+            pos = string.search(/[+x÷-]+/g);
+        }
+        
+
+        if (pos != -1)
+        {   
+            if(pos==0)
+            {
+                string = string.slice(1)
+                num1 = "-"+string.slice(0,string.search(/[+x÷-]+/g))
+                string = string.slice(string.search(/[+x÷-]+/g));
+                
+            }
+            else{
+                num1 = string.slice(0,pos);
+                string = string.slice(pos);
+            }
+            
 
             operator = string.slice(0,1);
             string = string.slice(1);
@@ -53,6 +83,7 @@ function operate(string){
 
 
         }
+
         console.log("pos: " + pos);
         console.log("Num1: " + num1);
         console.log("Num2: " + num2);
@@ -61,8 +92,11 @@ function operate(string){
         console.log("");
     }   
 
-    string = Number(string.toPrecision(9));
+    console.log("final result "+string + " " + typeof string)
+    string = Number(Number(string).toPrecision(9));
     largeDisplay.innerText = string;
+    repeat2 = true;
+    resultPrint = true;
     
 }
 
@@ -70,6 +104,8 @@ function buttons(){
     const buttons = document.querySelectorAll(".row > div");
     const smallDisplay = document.querySelector("#smallDisplay");
     const largeDisplay = document.querySelector("#largeDisplay");
+    let repeat = true;
+    resultPrint = false;
 
 
     buttons.forEach(button => {
@@ -84,21 +120,73 @@ function buttons(){
         else if (button.innerText == "=")
         {   
             button.addEventListener("click",function(e){
-                smallDisplay.innerText += largeDisplay.innerText
-                operate(smallDisplay.innerText);
+                if (repeat == false)
+                {
+                    smallDisplay.innerText += largeDisplay.innerText;
+                    repeat = true;
+                    operate(smallDisplay.innerText);
+                }
+                else
+                {
+                    smallDisplay.innerText = largeDisplay.innerText
+                }
+
+    
+
+            })
+        }
+
+        else if (button.innerText == "±")
+        {   
+            button.addEventListener("click",function(e){
+                if (largeDisplay != "")
+                {   
+                    if (largeDisplay.innerText.slice(0,1) == "-")
+                    {
+                        largeDisplay.innerText = largeDisplay.innerText.slice(1);
+                    }
+                    else
+                    {
+                        largeDisplay.innerText = "-"+largeDisplay.innerText;
+                    }
+
+                    
+                }
             })
         }
 
         else if (button.innerText.search(/[+x÷-]+/g) > -1)
         {   
             button.addEventListener("click",function(e){
-                smallDisplay.innerText += largeDisplay.innerText + e.target.innerText
+                if (smallDisplay.innerText.slice(-1).search(/[+x÷-]+/g) > -1)
+                {
+                    return;
+                }
+
+                if (e.target.innerText=="-" && smallDisplay.innerText=="" && largeDisplay.innerText=="")
+                {
+                    return;
+                }
+
+                
+
+                if (resultPrint == true)
+                {
+                    smallDisplay.innerText += e.target.innerText;
+                }
+                else{
+                    
+                    smallDisplay.innerText += largeDisplay.innerText + e.target.innerText
+                }
+
+                repeat = false;
                 largeDisplay.innerText = "";
             })
         }
 
         else{
             button.addEventListener("click",function(e){
+                resultPrint = false;
                 largeDisplay.innerText += e.target.innerText;
             })
         }
